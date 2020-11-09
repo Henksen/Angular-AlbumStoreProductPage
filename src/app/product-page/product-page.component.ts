191e75494191112dfa0bc351ecd7f6c6091dd7d4
Pierik <henk-jan.pierik@rabobank.nl>
2020-11-09 12:42:16 +0100

    j

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ProductService} from '../product.service';
 
 @Component({
   selector: 'app-product-page',
@@ -7,9 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _productService: ProductService) { }
 
   ngOnInit() {
   }
-
 }
